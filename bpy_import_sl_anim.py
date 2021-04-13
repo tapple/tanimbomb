12086d4e7a7aaedd87a644077b03daae9af1e991
Tapple Gao <tapplek@gmail.com>
2021-04-12 22:12:43 -0500

    removed extra print statements that slow down import

--- bpy_import_sl_anim.py
+++ bpy_import_sl_anim.py
@@ -184,7 +184,7 @@ class JointMotion(object):
         bone_rot_inv = bone_rot.inverted()
         if self.rotKeys.size:
             data_path = ('pose.bones["%s"].rotation_quaternion' % self.name)
-            print("data_path = %s" % data_path)
+            # print("data_path = %s" % data_path)
             fw = action.fcurves.new(data_path, 0, self.name)
             fx = action.fcurves.new(data_path, 1, self.name)
             fy = action.fcurves.new(data_path, 2, self.name)
@@ -192,7 +192,7 @@ class JointMotion(object):
             for t, x, y, z in self.get_rotKeysF(dur):
                 w2 = 1 - x*x - y*y - z*z
                 w = math.sqrt(w2) if w2 > 0 else 0
-                print("%ft %fw %fx %fy %fz" % (t, w, x, y, z))
+                # print("%ft %fw %fx %fy %fz" % (t, w, x, y, z))
                 q = Quaternion((w, y, -x, z))
                 q = bone_rot_inv * q * bone_rot
                 fw.keyframe_points.insert(t, q.w)
@@ -201,12 +201,12 @@ class JointMotion(object):
                 fz.keyframe_points.insert(t, q.z)
         if self.locKeys.size:
             data_path = 'pose.bones["%s"].location' % self.name
-            print("data_path = %s" % data_path)
+            # print("data_path = %s" % data_path)
             fx = action.fcurves.new(data_path, 0, self.name)
             fy = action.fcurves.new(data_path, 1, self.name)
             fz = action.fcurves.new(data_path, 2, self.name)
             for t, x, y, z in self.get_locKeysF(dur):
-                print("%ft %fx %fy %fz" % (t, x, y, z))
+                # print("%ft %fx %fy %fz" % (t, x, y, z))
                 v = Vector((y, -x, z))
                 v.rotate(bone_rot_inv)
                 fx.keyframe_points.insert(t, v.x)
