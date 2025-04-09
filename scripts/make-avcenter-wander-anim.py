ae975784f04b7d0c6fb191759461c1f8e09fe482
tapple <tapplek@gmail.com>
2025-04-09 12:11:59 -0700

    some progress in autogenerating avcenter wander anims

--- scripts/make-avcenter-wander-anim.py
+++ scripts/make-avcenter-wander-anim.py
@@ -1,5 +1,6 @@
+from math import sin, cos, pi
 import animDump
-import numpy
+import numpy as np
 
 def enumerate_waypoints():
     RADIUS = 1.5/2
@@ -8,13 +9,26 @@ def enumerate_waypoints():
     RINGS = 2
     for r in range(1, RINGS+1):
         for i in range(POINTS):
-            angle = 2 * numpy.pi / POINTS * (i + PHASE)
-            offset = numpy.array([0, numpy.cos(angle), numpy.sin(angle), 0]) * r * RADIUS
-            angle2 = 2 * numpy.pi / POINTS * (i+1 + PHASE)
-            offset2 = numpy.array([0, numpy.cos(angle2), numpy.sin(angle2), 0]) * r * RADIUS
+            angle = 2 * pi / POINTS * (i + PHASE)
+            offset = np.array([0, cos(angle), sin(angle), 0]) * r * RADIUS
+            angle2 = 2 * pi / POINTS * (i+1 + PHASE)
+            offset2 = np.array([0, cos(angle2), sin(angle2), 0]) * r * RADIUS
             for s in range(r):
                 interp = (offset2 * s + offset * (r - s)) / r
                 yield f"{r}{6*i+3*s:02d}", interp
+
+def enumerate_headings():
+    PHASE = 0
+    POINTS = 12
+    for i in range(POINTS):
+        t = i / POINTS
+        x = sin(pi*t)  # pi not tau because quaternion double cover
+        w = cos(pi*t)
+        if w < 0:
+            x = -x
+        angle = pi / POINTS * (i + PHASE)
+        offset = np.array([0, 0, 0, x])
+        yield f"{3*i:02d}", offset
 
 def make_avatar_center_pos_anim(name, pos):
     anim = animDump.KeyframeMotion(priority=2, easeIn=2.0, easeOut=0.1, loop_start=0, loop_end=0, duration=0)
@@ -26,6 +40,20 @@ def make_avatar_center_pos_anims():
         make_avatar_center_pos_anim(name, pos)
     make_avatar_center_pos_anim("000", [0,0,0,0])
 
+
+def make_avatar_center_rot_anim(name, rot):
+    anim = animDump.KeyframeMotion(priority=2, easeIn=0.5, easeOut=0.1, loop_start=0, loop_end=0, duration=0)
+    anim.ensure_joint('Avatar Center', rotKeysF=[rot])
+    anim.serialize_filename(f'AvCR{name}.anim')
+
+def make_avatar_center_rot_anims():
+    for name, rot in enumerate_headings():
+        make_avatar_center_rot_anim(name, rot)
+
 # for name, pos in enumerate_waypoints():
 #     print(f"{name}: {pos}")
-make_avatar_center_pos_anims()
+# make_avatar_center_pos_anims()
+
+# for name, pos in enumerate_headings():
+#     print(f"{name}: {pos}")
+make_avatar_center_rot_anims()
