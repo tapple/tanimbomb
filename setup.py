8c7396325822f3a406869555f3b2620f9bcd93f5
tapple <tapplek@gmail.com>
2025-08-02 07:44:37 -0700

    convert setup.py to pyproject.toml

--- setup.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python
-from setuptools import setup
-setup(name='tanimbomb',
-    entry_points={'console_scripts': ['animDump = animDump:main']},
-    py_modules=["animDump"],
-    install_requires=['numpy'],
-)
